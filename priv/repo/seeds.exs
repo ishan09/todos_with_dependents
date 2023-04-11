e4655d2c288697b21b7bf7be4b2417ab9df7f48e
ishan09 <ishansharma191@gmail.com>
2023-04-11 18:20:54 +0000

    Updated Readme

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,4 +10,41 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-TodoWithDependentTask.Setup.run()
+    task_group_1 = %TaskGroup{} |> TaskGroup.changeset(%{title: "Task Group 1"}) |> Repo.insert!()
+
+    task_group_2 = %TaskGroup{} |> TaskGroup.changeset(%{title: "Task Group 2"}) |> Repo.insert!()
+
+    task_1_1 =
+      %Task{}
+      |> Task.changeset(%{
+        description: "Completed Task",
+        is_completed: true,
+        task_group_id: task_group_2.id
+      })
+      |> Repo.insert!()
+
+    %Task{}
+    |> Task.changeset(%{
+      description: "Locked Task",
+      task_group_id: task_group_2.id,
+      child_tasks: [
+        %{description: "Incomplete Task", task_group_id: task_group_2.id}
+      ]
+    })
+    |> Repo.insert!()
+
+    %Task{}
+    |> Task.changeset(%{
+      description: "Task 3",
+      is_completed: false,
+      task_group_id: task_group_1.id,
+      child_tasks: [
+        %{description: "Task 3.1", task_group_id: task_group_1.id, is_completed: true},
+        %{
+          description: "Task 3.2",
+          task_group_id: task_group_1.id,
+          child_tasks: [%{description: "Task 3.2.1", task_group_id: task_group_1.id}]
+        }
+      ]
+    })
+    |> Repo.insert!()
